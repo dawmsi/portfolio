766bf6da5205fe23cae30f59afe11247cdfd5486
dwmsi <da._.off@outlook.com>
2022-11-08 23:02:27 +0200

    nav change touch buttons areas

--- src/containers/NavBar/index.js
+++ src/containers/NavBar/index.js
@@ -16,35 +16,47 @@ export const NavBar = ({ isLight, changeTheme }) => {
         <>
             <Navbar expand="sm" bg={isLight ? 'light' : 'dark'}>
                 <Container className="flex-nowrap">
-                    <Nav fill navbarScroll={true} variant="pills">
-                        <Nav.Link eventKey="about" as={Link} to={'/'}>
-                            <Button variant={isLight ? 'light' : 'dark'}>
-                                <Person />
-                            </Button>
-                        </Nav.Link>
-                        <Nav.Link eventKey="skills" as={Link} to={'/skills'}>
-                            <Button variant={isLight ? 'light' : 'dark'}>
-                                <Diagram2 />
-                            </Button>
-                        </Nav.Link>
-                        <Nav.Link
-                            eventKey="projects"
-                            as={Link}
-                            to={'/projects'}
-                        >
-                            <Button variant={isLight ? 'light' : 'dark'}>
-                                <Grid />
-                            </Button>
-                        </Nav.Link>
-                        <Nav.Link
-                            eventKey="statistic"
-                            as={Link}
-                            to={'/statistic'}
-                        >
-                            <Button variant={isLight ? 'light' : 'dark'}>
-                                <Bezier2 />
-                            </Button>
-                        </Nav.Link>
+                    <Nav navbarScroll={true} variant="pills">
+                        <div className="flex-fill">
+                            <Nav.Link eventKey="about" as={Link} to={'/'}>
+                                <Button variant={isLight ? 'light' : 'dark'}>
+                                    <Person />
+                                </Button>
+                            </Nav.Link>
+                        </div>
+                        <div className="flex-fill">
+                            <Nav.Link
+                                eventKey="skills"
+                                as={Link}
+                                to={'/skills'}
+                            >
+                                <Button variant={isLight ? 'light' : 'dark'}>
+                                    <Diagram2 />
+                                </Button>
+                            </Nav.Link>
+                        </div>
+                        <div className="flex-fill">
+                            <Nav.Link
+                                eventKey="projects"
+                                as={Link}
+                                to={'/projects'}
+                            >
+                                <Button variant={isLight ? 'light' : 'dark'}>
+                                    <Grid />
+                                </Button>
+                            </Nav.Link>
+                        </div>
+                        <div className="flex-fill">
+                            <Nav.Link
+                                eventKey="statistic"
+                                as={Link}
+                                to={'/statistic'}
+                            >
+                                <Button variant={isLight ? 'light' : 'dark'}>
+                                    <Bezier2 />
+                                </Button>
+                            </Nav.Link>
+                        </div>
                     </Nav>
 
                     <Button
