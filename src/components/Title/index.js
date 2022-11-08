a8240782d4528e8d11ac58166ca14eb331c26f07
dwmsi <da._.off@outlook.com>
2022-11-08 17:14:45 +0200

    Breadcrumb

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,29 +1,71 @@
 import React from 'react'
 import { Breadcrumb, Col, Container, Row } from 'react-bootstrap'
+import { useLocation, Link } from 'react-router-dom'
 import './Title.scss'
 export const Title = ({ isLight }) => {
-    return (
-        <>
-            <Col className="title__col">
-                <Container className="title__container">
-                    <Row
-                        className={
-                            isLight
-                                ? 'title__row bg-light text-dark'
-                                : ' title__row bg-dark text-light'
-                        }
-                    >
-                        <Breadcrumb className="title__col">
-                            <Breadcrumb.Item
-                                className={isLight ? 'text-dark' : 'text-light'}
-                                active
-                            >
-                                now
-                            </Breadcrumb.Item>
-                        </Breadcrumb>
-                    </Row>
-                </Container>
-            </Col>
-        </>
-    )
+    const location = useLocation()
+    const breadCrumbView = () => {
+        const { pathname } = location
+        const pathnames = pathname.split('/').filter((item) => item)
+        const capatilize = (s) => s.charAt(0).toUpperCase() + s.slice(1)
+        return (
+            <>
+                <Col className="title__col">
+                    <Container className="title__container">
+                        <Row
+                            className={
+                                isLight
+                                    ? 'title__row bg-light text-dark'
+                                    : ' title__row bg-dark text-light'
+                            }
+                        >
+                            <Breadcrumb className="title__col">
+                                {pathnames.length > 0 ? (
+                                    <Breadcrumb.Item
+                                        linkAs={Link}
+                                        linkProps={{ to: '/' }}
+                                        className={
+                                            isLight ? 'text-dark' : 'text-light'
+                                        }
+                                    >
+                                        About
+                                    </Breadcrumb.Item>
+                                ) : (
+                                    <Breadcrumb.Item active>
+                                        About
+                                    </Breadcrumb.Item>
+                                )}
+                                {pathnames.map((name, index) => {
+                                    const routeTo = `/${pathnames
+                                        .slice(0, index + 1)
+                                        .join('/')}`
+                                    const isLast =
+                                        index === pathnames.length - 1
+                                    return isLast ? (
+                                        <Breadcrumb.Item key={name} active>
+                                            {capatilize(name)}
+                                        </Breadcrumb.Item>
+                                    ) : (
+                                        <Breadcrumb.Item
+                                            key={name}
+                                            linkAs={Link}
+                                            linkProps={{ to: `${routeTo}` }}
+                                            className={
+                                                isLight
+                                                    ? 'text-dark'
+                                                    : 'text-light'
+                                            }
+                                        >
+                                            {capatilize(name)}
+                                        </Breadcrumb.Item>
+                                    )
+                                })}
+                            </Breadcrumb>
+                        </Row>
+                    </Container>
+                </Col>
+            </>
+        )
+    }
+    return <>{breadCrumbView()}</>
 }
