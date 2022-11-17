d57bf6a72f3a492bc3c5f4bef5b91a9d64d57280
dwmsi <da._.off@outlook.com>
2022-11-17 23:40:34 +0200

    small fixing

--- src/scenes/Projects/index.js
+++ src/scenes/Projects/index.js
@@ -8,14 +8,15 @@ export const Projects = ({ isCarouseled, isLight }) => {
         {
             id: 1,
             name: 'Modest',
-            description: 'Blog site',
+            description: 'Blog site, flex-box styling, google map, contact form',
             link: 'https://dawmsi.github.io/blog-project/',
             image: 'https://www.site-shot.com/cached_image/4di4QGacEe2oDAJCrBEAAg'
         },
         {
             id: 2,
             name: 'Weather',
-            description: 'search by city',
+            description:
+                'search by city, practice clear js, fetch, work with API, ',
             link: 'https://dawmsi.github.io/weather/',
             image: 'https://www.site-shot.com/cached_image/hseRYmafEe2TkQJCrBEAAw'
         }
