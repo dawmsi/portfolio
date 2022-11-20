7157f04b166c8bdeb6f98c1dababea62d0a433d4
dwmsi <da._.off@outlook.com>
2022-11-20 17:46:54 +0200

    some issue i think

--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -10,11 +10,15 @@ import { Main } from '../containers/Main'
 export const MainRoutes = ({ isLight }) => {
     return (
         <Routes>
-            <Route path="/" element={<Navigate to="/portfolio" replace />} />
-            <Route path="/portfolio/*" render={() => <Main isLight={isLight} />} />
+            <Route path="*" element={<Navigate to="/portfolio" replace />} />
             <Route path="/portfolio" element={<Main isLight={isLight} />}>
                 <Route index element={<Welcome isLight={isLight} />} />
-                <Route path="about" element={<About isLight={isLight} />} anima/>
+
+                <Route
+                    path="about"
+                    element={<About isLight={isLight} />}
+                    anima
+                />
                 <Route path="skills" element={<Skills isLight={isLight} />} />
                 <Route
                     path="projects"
