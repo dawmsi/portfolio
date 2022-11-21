2a16532c450a35239eb0b640b1475c74fe483f03
dwmsi <da._.off@outlook.com>
2022-11-21 20:59:33 +0200

    remove icon

--- src/components/ClickableButton/ClickableButton.js
+++ src/components/ClickableButton/ClickableButton.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import { Button } from 'react-bootstrap'
-import { ArrowCounterclockwise } from 'react-bootstrap-icons'
 
 export const ClickableButton = ({
     id,
