07febf11a3396d06e4b93cadf6c2105a716cbde7
dwmsi <da._.off@outlook.com>
2022-11-22 11:05:50 +0200

    default btn & btns style in componet

--- src/components/UnclickableButton/UnclickableButton.js
+++ src/components/UnclickableButton/UnclickableButton.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Button } from 'react-bootstrap'
+import './Unclickable.scss'
 export const UnclickableButton = ({ id, isLight, inside }) => {
     return (
         <>
             <Button
                 disabled
                 variant={isLight ? 'dark' : 'light'}
-                className="about__btn"
+                className="unclickeble__btn"
                 id={id}
             >
                 {inside}
