ba9e58d5e827d3cc0f2d832d61d579f414a91c12
Denys Shapoval <96021986+dawmsi@users.noreply.github.com>
2022-11-30 21:41:50 +0200

    back no cors

--- src/services/imageFetcher/imageFetcher.js
+++ src/services/imageFetcher/imageFetcher.js
@@ -2,7 +2,11 @@ export const imageFetcher = async (url, setFunc, setLoading) => {
     console.log('fet')
     const key = '446ad08e84a3d905eb24e3191f238c4e'
     let imageUrl = `https://api.screenshotlayer.com/api/capture?access_key=${key}&url=${url}/&viewport=1200x720&width=720`
-    fetch(imageUrl).then((res) => {
+    fetch(imageUrl, {
+        method: 'GET',
+        mode: 'no-cors',
+        headers: {}
+    }).then((res) => {
         if (res.status >= 400) {
             return res.url
         }
