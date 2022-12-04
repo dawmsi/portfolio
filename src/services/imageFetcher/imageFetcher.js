264a03237669992561f9b0192a6fc7dc1fa3217b
Denys Shapoval <96021986+dawmsi@users.noreply.github.com>
2022-12-04 19:18:51 +0200

    new image api key

--- src/services/imageFetcher/imageFetcher.js
+++ src/services/imageFetcher/imageFetcher.js
@@ -1,5 +1,5 @@
 export const imageFetcher = async (url, setFunc, setLoading) => {
-    const key = 'a9c4da9f5b0b90ecc9c1b1f56ecec1b5'
+    const key = 'b15c1068488d6e70e7255f7b5741f866'
     let imageUrl = `https://api.screenshotlayer.com/api/capture?access_key=${key}&url=${url}/&viewport=1200x720&width=720`
     fetch(imageUrl, {
         method: 'GET',
