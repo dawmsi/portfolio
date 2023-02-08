bd92a3d1d85ed028d38fecab9ff6d25de50c2625
Denys Shapoval <96021986+dawmsi@users.noreply.github.com>
2023-02-08 16:34:05 +0200

    conponent NavButton

---
+++ src/components/NavButton/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button, Nav } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
+export const NavButton = ({ isLight, to, eventKey, children }) => {
+    return (
+        <div className="flex-fill" key={to}>
+            <Nav.Link eventKey={eventKey} as={NavLink} to={to}>
+                <Button variant={isLight ? 'light' : 'dark'}>{children}</Button>
+            </Nav.Link>
+        </div>
+    )
+}
--- src/modules/NavBar/index.jsx
+++ src/modules/NavBar/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Container, Navbar, Nav, Button } from 'react-bootstrap'
-import { NavLink } from 'react-router-dom'
 import './NavBar.scss'
 import {
     Moon,
@@ -11,57 +10,44 @@ import {
     Bezier2,
     Search
 } from 'react-bootstrap-icons'
+import { NavButton } from '../../components/NavButton'
+
 export const NavBar = ({ isLight, changeTheme }) => {
     return (
         <>
             <Navbar expand="sm" bg={isLight ? 'light' : 'dark'}>
                 <Container className="flex-nowrap">
                     <Nav navbarScroll={true} variant="pills">
-                        <div className="flex-fill">
-                            <Nav.Link
-                                eventKey="/"
-                                as={NavLink}
-                                to={'/portfolio/'}
-                            >
-                                <Button variant={isLight ? 'light' : 'dark'}>
-                                    <Search />
-                                </Button>
-                            </Nav.Link>
-                        </div>
-                        <div className="flex-fill">
-                            <Nav.Link
-                                eventKey="about"
-                                as={NavLink}
-                                to={`/portfolio/about`}
-                            >
-                                <Button variant={isLight ? 'light' : 'dark'}>
-                                    <Person />
-                                </Button>
-                            </Nav.Link>
-                        </div>
-                        <div className="flex-fill">
-                            <Nav.Link
-                                eventKey="skills"
-                                as={NavLink}
-                                to={'/portfolio/skills'}
-                            >
-                                <Button variant={isLight ? 'light' : 'dark'}>
-                                    <Diagram2 />
-                                </Button>
-                            </Nav.Link>
-                        </div>
-                        <div className="flex-fill">
-                            <Nav.Link
-                                eventKey="projects"
-                                as={NavLink}
-                                to={'/portfolio/projects'}
-                            >
-                                <Button variant={isLight ? 'light' : 'dark'}>
-                                    <Grid />
-                                </Button>
-                            </Nav.Link>
-                        </div>
-                        <div className="flex-fill">
+                        <NavButton
+                            isLight={isLight}
+                            to={'/portfolio/'}
+                            eventKey="/"
+                        >
+                            <Search />
+                        </NavButton>
+                        <NavButton
+                            isLight={isLight}
+                            to={`/portfolio/about`}
+                            eventKey="about"
+                        >
+                            <Person />
+                        </NavButton>
+                        <NavButton
+                            isLight={isLight}
+                            to={'/portfolio/projects'}
+                            eventKey="projects"
+                        >
+                            <Grid />
+                        </NavButton>
+                        <NavButton
+                            isLight={isLight}
+                            eventKey="skills"
+                            to={'/portfolio/skills'}
+                        >
+                            <Diagram2 />
+                        </NavButton>
+
+                        {/*                         <div className="flex-fill">
                             <Nav.Link
                                 eventKey="statistic"
                                 as={NavLink}
@@ -71,7 +57,7 @@ export const NavBar = ({ isLight, changeTheme }) => {
                                     <Bezier2 />
                                 </Button>
                             </Nav.Link>
-                        </div>
+                        </div> */}
                     </Nav>
                     <Button
                         className="mode__button px-8"
